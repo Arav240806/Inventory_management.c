f8726321f531de4de8f7febfa3cde346c1bd2c78
Arav240806 <shrivastavarav41@gmail.com>
2025-06-20 00:07:24 +0530

    Update and rename README.md to Inventory_Management.c

---
+++ Inventory_Management.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+typedef struct display_product{
+int product_no;
+int Quantity;
+int price;
+char update_product[10];
+char product_name[30];
+} Item ;
+int main()
+{
+char buisness_name[50];
+char buisness_type[50];
+Item item;
+printf("Welcome to the Inventory Management System\n");
+printf("Enter Buisness Name:\n");
+scanf("%s",&buisness_name);
+printf("Enter Buisness Type(Manufacturers/Distributors)\n");
+scanf("%s",&buisness_type);
+printf("Enter choice to add products\n");
+int choice;
+printf("Enter choice\n");
+scanf("%d",&choice);
+printf("Product name\n");
+scanf("%s",item.product_name);
+
+
+printf("No of product\n");
+scanf("%d",&item.product_no);
+
+
+printf("Product Quantity\n");
+scanf("%d",&item.Quantity);
+
+printf("Product price\n");
+scanf("%d",&item.price);
+
+if(item.Quantity>0)
+{
+printf("Available");
+}
+else{
+printf("Not Available");
+}
+
+
+char action[10];
+printf("Do you want to edit or continue\n");
+scanf("%s",action);
+if(strcmp(action,"edit")==0){
+printf("You choose edit option");
+}
+else if (strcmp(action,"continue")==0){
+printf("You choose continue option");
+}
+else{
+printf("Exit Program\n");
+printf("Thank you\n");
+exit(0);
+}
+
+return 0;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Inventory_management.c
-A Basic Inventory Management System project created in C language using mobile. Focused on practicing structures, inputs ,and logic. 
